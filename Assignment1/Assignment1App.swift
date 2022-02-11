08056e4c1181c0c2caee8beeea0e1f7f19001a11
viplav153 <viplav153@gmail.com>
2022-02-11 17:15:18 +0530

    initialise project

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by Viplav Anand on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment1/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Assignment1
+//
+//  Created by Viplav Anand on 11/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
